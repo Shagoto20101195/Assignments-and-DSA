eab65ceef5d05133f44b7152da0509971a660909
Andalib Rahman Shagoto <64375438+Shagoto20101195@users.noreply.github.com>
2022-10-15 10:41:41 +0600

    Add files via upload

---
+++ Assignment/460/Lab 3/example.v
@@ -0,0 +1,30 @@
+// Resetn is like active low. When it is 0, it will reset
+module example(Clock, Resetn, w, z);
+
+input Clock, Resetn, w;
+output z;
+reg [2:1] y, Y;
+parameter [2:1] A=2'b00, B=2'b01, C=2'b10;
+
+// Define the next combinational circuit
+// Small y = Present state
+// Capital Y = Next state
+always@(w,y)
+	case(y)
+		A: if(w) Y = B;
+			else Y = A;
+		B: if(w) Y = C;
+			else Y = A;
+		C: if(w) Y = C;
+			else Y = A;
+		default: Y = 2'bxx;
+	endcase
+
+// Define the sequential block
+always@(negedge Resetn, posedge Clock)
+	if(Resetn == 0) y<=A;
+	else y<=Y;
+
+// Define output
+assign z = (y == C);
+endmodule
